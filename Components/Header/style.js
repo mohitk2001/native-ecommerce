06beec192dea78f1804d2ff68fa8927433fec006
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2022-10-13 00:01:30 +0530

    Oct 13, 2022

--- Components/Header/style.js
+++ Components/Header/style.js
@@ -4,7 +4,15 @@ const styles=StyleSheet.create({
     Header_Container:{
         width:"100%",
         display:"flex",
-        alignItems:"center"
+        alignItems:"center",
+        justifyContent:"center",
+        height:60,
+        // borderColor:"red",
+        // borderBottomWidth:2,
+        // shadowColor: '#171717',
+        // shadowOffset: {width: -2, height: 4},
+        // shadowOpacity: 0.2,
+        // shadowRadius: 3,
     },
     Header_sub_container:{
         width:"95%",
@@ -20,11 +28,12 @@ const styles=StyleSheet.create({
         
     }
     ,Header_Right:{
-        marginHorizontal:10
+        marginHorizontal:10,
+        color:"#ff6699"
     },
     Header_Icon_Size:{
         fontSize:30,
-        color:"#ff0066"
+        color:"#ff5500"
     },
     Header_userProfile:{
         backgroundColor:"red"
---
+++ Components/HighLightTop/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles=StyleSheet.create({
+    HightLight_Container:{
+        width:"100%",
+        marginTop:20,
+        display:'flex',
+        alignItems:"center"
+    }
+    ,
+    HightLight_Container_Image:{
+        width:"90%",
+      
+    }
+
+})
+
+export default styles
